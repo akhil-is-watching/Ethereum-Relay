0f341e0a13bedf642907c4f655e27811ec7bb587
akhil-is-watching <praj2k2@protonmail.com>
2023-01-30 11:21:55 +0530

    Updated test script

--- src/test/test.py
+++ src/test/test.py
@@ -1,6 +1,7 @@
 import concurrent.futures
 import json
 import requests
+import sys
 
 data = {
   "to": "0xa65309c4a9398946A86862Dee81f05Cde9a3958D",
@@ -16,13 +17,15 @@ def post_request(url):
     response = requests.post(url, data=json.dumps(data), headers=headers)
     return response
 
+count = 0;
 with concurrent.futures.ThreadPoolExecutor() as executor:
     # Use map to send the requests concurrently
-    results = [executor.submit(post_request, 'http://localhost:1337/transaction/sendTransaction') for _ in range(500)]
+    results = [executor.submit(post_request, 'http://localhost:1337/transaction/sendTransaction') for _ in range(int(sys.argv[1]))]
 
     for f in concurrent.futures.as_completed(results):
         try:
             response = f.result()
-            print(response.status_code)
+            print(count)
+            count += 1
         except Exception as e:
             print("Error: ", e)
